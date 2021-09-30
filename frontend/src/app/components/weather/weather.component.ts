efa50fe111d86dbfe9b2e213dad1722e93bddd79
Sebastian <mrsebbymorris@gmail.com>
2021-09-30 19:47:21 +0200

    Front in progress and merged tests into that

--- frontend/src/app/components/weather/weather.component.ts
+++ frontend/src/app/components/weather/weather.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
+  public lang: string = "fr";
+  public username: string = "TODO";
+  public city: string = "Montcuq";
+  public strToPrint: string = "Bienvenue, " + this.username + " voici la météo de " + this.city + " :";
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  changeLang(lang): void {
+    let src: string = this.lang;
+    this.lang = lang;
+
+    //TODO: Appel API
+  }
+
+  changeCity(city): void {
+    switch (city){
+      case "Montcuq":
+        this.city = "";
+        break;
+      case "Belleville":
+        this.city = "";
+        break;
+      case "Le Fion":
+        this.city = "";
+        break;
+    }
+  }
+
+  updateStrToPrint(): void{
+    this.strToPrint = "";
+  }
 }
