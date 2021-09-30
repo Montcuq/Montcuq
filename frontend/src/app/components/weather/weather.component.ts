8e1b67c2c71f10d9bdbe97ae949119ebbe222ef5
Sebastian <mrsebbymorris@gmail.com>
2021-09-30 20:56:33 +0200

    All front almost done before API calls

--- frontend/src/app/components/weather/weather.component.ts
+++ frontend/src/app/components/weather/weather.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
   public lang: string = "fr";
-  public username: string = "TODO";
+  public username: string = this.getUsername();
   public city: string = "Montcuq";
   public strToPrint: string = "Bienvenue, " + this.username + " voici la météo de " + this.city + " :";
 
@@ -15,30 +15,47 @@ export class WeatherComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.printWeather(this.city);
+  }
+
+  getUsername(){
+    return "TODO";
   }
 
   changeLang(lang): void {
     let src: string = this.lang;
     this.lang = lang;
 
-    //TODO: Appel API
+    this.strToPrint = this.strToPrint; //TODO: Appel API
   }
 
   changeCity(city): void {
+    this.city = city;
+    this.strToPrint = "Bienvenue, " + this.username + " voici la météo de " + this.city + " :";
+    let currentLang = this.lang;
+    this.lang = "fr";
+    this.changeLang(currentLang);
+
     switch (city){
       case "Montcuq":
-        this.city = "";
+        this.printWeather("Montcuq");
         break;
-      case "Belleville":
-        this.city = "";
+      case "Anus":
+        this.printWeather("Fouronnes");
         break;
       case "Le Fion":
-        this.city = "";
+        this.printWeather("Chevenoz");
         break;
-    }
+      case "Duranus":
+        this.printWeather("Duranus");
+        break;
+      case "Froiscul":
+        this.printWeather("Moyeuvre-Grande");
+        break;
+      }
   }
 
-  updateStrToPrint(): void{
-    this.strToPrint = "";
+  printWeather(city){
+    //Appel API
   }
 }
