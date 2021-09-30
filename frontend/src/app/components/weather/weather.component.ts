c08dfcb24675e9941bcce2bfc7c8be03aa577db8
Vincent Letourmy <letourmyvincent@outlook.fr>
2021-09-30 20:50:58 +0200

    Appel api du front weather et translate OK

--- frontend/src/app/components/weather/weather.component.ts
+++ frontend/src/app/components/weather/weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {WeatherService} from "../../services/weather.service";
 
 @Component({
   selector: 'app-weather',
@@ -11,7 +12,7 @@ export class WeatherComponent implements OnInit {
   public city: string = "Montcuq";
   public strToPrint: string = "Bienvenue, " + this.username + " voici la météo de " + this.city + " :";
 
-  constructor() {
+  constructor(private weatherService: WeatherService) {
   }
 
   ngOnInit(): void {
