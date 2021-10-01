9d89ee3df8c9ef0f92e04585a9a8c7ad730a5796
Evi-Raoul <evi.raoul@hotmail.fr>
2021-10-01 20:10:00 +0200

    docker compose fonctionne

--- serveur/src/mongo/mongo.js
+++ serveur/src/mongo/mongo.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb//172.27.0.3:27017/test',
-    { useNewUrlParser: true,
-        useUnifiedTopology: true,
-        user: 'root',
-        pass: 'example'})
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://root:Montcuq@mongodb:27017',    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .then(() => console.log(' Connexion à MongoDB:ecarlate réussie !'.green))
     .catch((e) => console.log(e));
-//    .catch(() => console.log(' Connexion à MongoDB:ecarlate échouée !'.red));
+    // .catch(() => console.log(' Connexion à MongoDB:ecarlate échouée !'.red));
